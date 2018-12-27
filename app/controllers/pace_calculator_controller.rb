adca0532a3764da4d33f61e9a38dd2f18050cd9e
Jennifer Konikowski <jennifer.konikowski@stitchfix.com>
2018-12-27 13:59:03 -0500

    add error message if required fields are not added

--- app/controllers/pace_calculator_controller.rb
+++ app/controllers/pace_calculator_controller.rb
@@ -5,42 +5,45 @@ class PaceCalculatorController < ApplicationController
 
   def pace
     if params[:pace]
-      @pace = Pace.new(type: "pace",
-                       activity: params[:activity],
-                       distance: params[:distance],
-                       pace_length: params[:pace_length],
-                       distance_length: params[:distance_length],
-                       time_hours: params[:time_hours],
-                       time_minutes: params[:time_minutes],
-                       time_seconds: params[:time_seconds])
+      if params[:distance].empty? || (params[:time_hours].empty? && params[:time_minutes].empty? && params[:time_seconds].empty?)
+        @error = "If calculating pace, distance and time are required."
+      else
+        @pace = Pace.new(type: "pace",
+                         activity: params[:activity],
+                         distance: params[:distance],
+                         pace_length: params[:pace_length],
+                         distance_length: params[:distance_length],
+                         time_hours: params[:time_hours],
+                         time_minutes: params[:time_minutes],
+                         time_seconds: params[:time_seconds])
+      end
     elsif params[:time]
-      @pace = Pace.new(type: "time",
-                       activity: params[:activity],
-                       distance: params[:distance],
-                       pace_length: params[:pace_length],
-                       distance_length: params[:distance_length],
-                       pace_minutes: params[:pace_minutes],
-                       pace_seconds: params[:pace_seconds])
+      if params[:distance].empty? || (params[:pace_minutes].empty? && params[:pace_seconds].empty?)
+        @error = "If calculating time, distance and pace are required."
+      else
+        @pace = Pace.new(type: "time",
+                         activity: params[:activity],
+                         distance: params[:distance],
+                         pace_length: params[:pace_length],
+                         distance_length: params[:distance_length],
+                         pace_minutes: params[:pace_minutes],
+                         pace_seconds: params[:pace_seconds])
+      end
     elsif params[:distance]
-      @pace = Pace.new(type: "distance",
-                       activity: params[:activity],
-                       pace_length: params[:pace_length],
-                       distance_length: params[:distance_length],
-                       pace_minutes: params[:pace_minutes],
-                       pace_seconds: params[:pace_seconds],
-                       time_hours: params[:time_hours],
-                       time_minutes: params[:time_minutes],
-                       time_seconds: params[:time_seconds])
+      if (params[:pace_minutes].empty? && params[:pace_seconds].empty?) || (params[:time_hours].empty? && params[:time_minutes].empty? && params[:time_seconds].empty?)
+        @error = "If calculating distance, pace and time are required."
+      else
+        @pace = Pace.new(type: "distance",
+                         activity: params[:activity],
+                         pace_length: params[:pace_length],
+                         distance_length: params[:distance_length],
+                         pace_minutes: params[:pace_minutes],
+                         pace_seconds: params[:pace_seconds],
+                         time_hours: params[:time_hours],
+                         time_minutes: params[:time_minutes],
+                         time_seconds: params[:time_seconds])
+      end
     end
     render 'pace_calculator/pace'
   end
-
-  def row
-
-  end
-
-  def swim
-  end
-
-
 end
